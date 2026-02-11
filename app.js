e02332f088e141a74779ad42768c9ac4908a5a02
thaistayandfly <thaistayandfly@gmail.com>
2026-02-11 22:40:37 +0700

    Update app.js
    update script URL

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // ===============================
 const CONFIG = {
   // Google Apps Script web app URL
-  API_URL: 'https://script.google.com/macros/s/AKfycbxNpKE2NdocchbIJfkAbVBRSfluP2Szaw_QceCm1QWrTE2xBVaDvCo7A1Wap9SkI6WPIA/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbyLaEDJI7LmMtcueaAwJBtXRKA3Hy7i3rXTtet8z9uHoh68u4jLe84HJwZ_ISm4ExSzZw/exec',
   CACHE_KEY: 'itinerary_cache',
   CACHE_VERSION: 'v1',
   DB_NAME: 'TravelItineraryDB',
